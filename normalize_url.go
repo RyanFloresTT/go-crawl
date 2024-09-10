92ea037c9066068a370cd67cb7b6aad1a6fc0037
Ryan Flores <rryanflorres@gmail.com>
2024-09-10 16:59:11 -0700

    add html parser func and test (start)

--- normalize_url.go
+++ normalize_url.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func NormalizeURL(base string) (string, error) {
@@ -18,4 +21,28 @@ func NormalizeURL(base string) (string, error) {
 	}
 
 	return normalizedURL, nil
+}
+
+func GetURLsFromHTML(htmlbody, rawBaseURL string) ([]string, error) {
+	var urlResults []string
+
+	doc, err := html.Parse(strings.NewReader(htmlbody))
+	if err != nil {
+		return urlResults, err
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			tagURL := n.Attr[0].Val
+			urlResults = append(urlResults, tagURL)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+
+	return urlResults, nil
 }
